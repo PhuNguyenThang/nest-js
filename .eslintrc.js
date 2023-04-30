96d77aed7a7f95ea9e8bf8b3b327564591b013a8
PhuNguyenThang <thangphu103@gmail.com>
2023-04-30 10:21:44 +0700

    update setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,15 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaVersion: 2018,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin','prettier', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'eslint:recommended',
+    'prettier',
+    'airbnb-typescript/base',
   ],
   root: true,
   env: {
@@ -27,6 +31,19 @@ module.exports = {
         'endOfLine': 'auto'
       },
       { 'usePrettierrc': true }
-    ]
+    ],
+    // General ESLint rules
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+
+    // Import/export rules
+    'import/no-cycle': 'error',
+    'import/no-unresolved': 'off',
+  
+    // TypeORM specific rules
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error',
   },
 };
